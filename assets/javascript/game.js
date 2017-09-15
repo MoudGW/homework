ca1abd910ea524e83327575cba9f1e413b3c5e4d
mohamed mahmoud jedeine <moud@mohameds-MacBook-Air.local>
2017-09-15 01:27:26 -0400

    last commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,49 +4,41 @@ var numResults = 0;
 var startYear = 0;
 var endYear = 0;
 $("document").ready(function(){
-$("input[name='Number']").click(function(){
-$("input[name='Number']").val('');
-});
-$("#search").click(function(){
-  console.log("you here");
-search=$("input[name='search']").val();
-console.log(search);
-numResults=$("input[name='Number']").val();
-console.log(numResults);
-startYear=$("input[name='year']").val()+"0101";
-endYear=$("input[name='endYear']").val()+"0101"; 
-if(endYear==="0101" || startYear==="0101")
-{
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey +'&q='+search;
-}
-else{
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey +'&q='+search+ "&begin_date="+startYear +'&end_date='+endYear;
-}
+  $("input[name='Number']").click(function(){
+    $("input[name='Number']").val('');
+  });
+  $("#search").click(function(){
+    $('#result').html('<p>Top Articles</p>');
+    search=$("input[name='search']").val();
+    numResults=$("input[name='Number']").val();
+    startYear=$("input[name='year']").val()+"0101";
+    endYear=$("input[name='endYear']").val()+"0101"; 
+    if(endYear==="0101" || startYear==="0101")
+    {
+      var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey +'&q='+search;
+    }
+    else{
+      var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey +'&q='+search+ "&begin_date="+startYear +'&end_date='+endYear;
+    }
     
-$.ajax({
+    $.ajax({
      url: queryURL,
-    method: "GET"
-    }).done(function(response) {
+     method: "GET"
+   }).done(function(response) {
 
-       console.log("-----");
-        console.log("URL: " + queryURL);
-        console.log("-----");
+    var articles=response.response.docs;
 
-       console.log(numResults);
-      var articles=response.response.docs;
-
-   for (var i = 0; i < numResults; i++) {
-          
-        
-         $('#result').append("<div id="+i+">"+"<p class='arti'>"+(i+1)+"</p>"+articles[i].headline.main+"</div>");
-         $('#'+i).append("<div>"+articles[i].snippet+"</div>");
-         $('#'+i).append("<a href="+articles[i].web_url+">"+articles[i].web_url+"</a>");
+    for (var i = 0; i < numResults; i++) 
+   {
+     $('#result').append("<div id="+i+">"+"<p class='arti'>"+(i+1)+"</p>"+articles[i].headline.main+"</div>");
+     $('#'+i).append("<div>"+articles[i].snippet+"</div>");
+     $('#'+i).append("<p>Year : "+(articles[i].pub_date).substring(0,4)+"</p>");
+     $('#'+i).append("<a href="+articles[i].web_url+">"+articles[i].web_url+"</a>");
    }
-});
  });
-$("#clear").click(function(){
-  $("input[type=text]").val('');
-});
-
-
  });
+  $("#clear").click(function(){
+    $("input[type=text]").val('');
+    $('#result').html('<p>Top Articles</p>');
+  }); 
+});
